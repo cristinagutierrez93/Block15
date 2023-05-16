d66d6aa12d91f2a64e1c1ccce44c00243a54eb2a
Cristina Gutierrez <cristinagutierrez@Cristinas-MBP.lan>
2023-05-16 13:54:14 -0500

    block15

---
+++ Tuesday_index.js
@@ -0,0 +1,18 @@
+const dinner= {
+    cheeseburger: 12,
+    steak: 20,
+    soup: 8,
+    macAndCheese: 14,
+    soupAndSalad: 16,
+}
+console.log(Object.keys(dinner));
+console.log(Object.values(dinner));
+
+const prices = Object.values(dinner);
+let totalCost = 0;
+
+for (let i = 0; i < prices.length; i++) {
+    totalCost += prices[i];
+}
+
+console.log(totalCost);
